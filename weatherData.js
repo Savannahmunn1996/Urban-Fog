8d2760c672a1cae2444b71d8e136e36c084e5fa8
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-02-25 20:24:34 -0800

    button styled

--- weatherData.js
+++ weatherData.js
@@ -1,13 +1,13 @@
-function getWeather() {
-    let reqUrl="api.openweathermap.org/data/2.5/forecast?lat=52.52&lon=13.41&appid=5143d8e20964ed205db1c52c74ac73bb"
+var reqUrl="api.openweathermap.org/data/2.5/forecast?lat=34.052235&lon=118.2437&appid=5143d8e20964ed205db1c52c74ac73bb"
+fetch(reqUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+});
 
-    fetch(reqUrl)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-    })
-}
+   
 
-getWeather()
+
+
