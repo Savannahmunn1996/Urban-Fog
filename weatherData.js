20073e098f146b82d3b73d5abbaa2e4c08e80793
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-03-05 14:04:30 -0800

    local storage/ new images

--- weatherData.js
+++ weatherData.js
@@ -1,19 +1,19 @@
+searchButton = document.getElementById("search");
+restOfWeek = document.getElementById("restOfWeek");
+cityTitle = document.getElementById("cityTitle");
+dateToday = document.getElementById("date-today");
+tempToday = document.getElementById("temp-today");
+windToday = document.getElementById("wind-today");
+humidToday = document.getElementById("humidity-today");
+historyS = document.getElementById("history");
 
-searchButton = document.getElementById('search');
-restOfWeek = document.getElementById('restOfWeek');
-cityTitle = document.getElementById('cityTitle');
-dateToday = document.getElementById('date-today');
-tempToday = document.getElementById('temp-today');
-windToday = document.getElementById('wind-today');
-humidToday = document.getElementById('humidity-today');
-
-
-searchButton.addEventListener('click', function (event) {
+searchButton.addEventListener("click", function (event) {
   event.preventDefault();
-  input = document.getElementById('cityInput').value;
+  input = document.getElementById("cityInput").value;
   search(input);
-})
-
+  localStorage.setItem("lastSearch", `${input}`);
+  historyS.innerText = localStorage.getItem("lastSearch");
+});
 
 function search(cityName) {
   var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=5143d8e20964ed205db1c52c74ac73bb`;
@@ -32,14 +32,12 @@ function search(cityName) {
       dateToday.innerText = titleData.date;
       tempToday.innerText = `Temp: ${titleData.temp}`;
       windToday.innerText = `Wind: ${titleData.wind}`;
-      humidToday.innerText = `Humidity: ${titleData.humid}`
+      humidToday.innerText = `Humidity: ${titleData.humid}`;
 
-
-
-      const eleArr = mapArr.map(item => {
+      const eleArr = mapArr.map((item) => {
         return `
         <div class="day-card">
-        <img class="the-icon" src="images/windy.png">
+        <img class="the-icon" src="images/hot.png">
         <div>
             <p>Date: ${getData(item).date}</p>
         </div>
@@ -54,9 +52,9 @@ function search(cityName) {
         </div>
         </div>
         `;
-      })
+      });
 
-      restOfWeek.innerHTML = eleArr.join('');
+      restOfWeek.innerHTML = eleArr.join("");
       getData(data.list[0]);
     });
 }
@@ -64,11 +62,11 @@ function search(cityName) {
 function getData(data) {
   date = formatDate(data.dt_txt);
   kelvin = data.main.temp;
-  farenheit = Math.round(((kelvin - 273.15) * 9/5 + 32));
+  farenheit = Math.round(((kelvin - 273.15) * 9) / 5 + 32);
   temp = `${farenheit}\u00B0F`;
-  wind = `${data.wind.speed} mph`
-  humid = `${data.main.humidity}%`
-  return {date, temp, wind, humid}
+  wind = `${data.wind.speed} mph`;
+  humid = `${data.main.humidity}%`;
+  return { date, temp, wind, humid };
 }
 
 function formatDate(date) {
@@ -81,8 +79,11 @@ function formatDate(date) {
 function parseArray(data) {
   let ans = [data[0]];
   let i = 0;
-  while(ans.length < 6 || i < data.length) {
-    if(data[i].dt_txt.slice(0, 10) !== ans[ans.length - 1].dt_txt.slice(0, 10) && data[i].dt_txt.slice(11, data[i].dt_txt.length) == "12:00:00") {
+  while (ans.length < 6 || i < data.length) {
+    if (
+      data[i].dt_txt.slice(0, 10) !== ans[ans.length - 1].dt_txt.slice(0, 10) &&
+      data[i].dt_txt.slice(11, data[i].dt_txt.length) == "12:00:00"
+    ) {
       ans.push(data[i]);
     }
     i++;
@@ -90,5 +91,4 @@ function parseArray(data) {
   return ans;
 }
 
-
-search('Los Angeles');
+search("Los Angeles");
